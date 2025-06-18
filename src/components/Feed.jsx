7a0a5af9765e51d3bbc2988c002d66062eb6585d
Asha Laxmi <ashalaxmi@centizen.com>
2025-06-18 10:44:40 +0530

    changes in the feed page reponse.

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -16,7 +16,7 @@ const Feed = () => {
     if(feed) return;
     try {
       let res = await axios.get(`${BASE_URL}/user/feed`,{withCredentials:true});
-      dispatch(addFeed(res.data));
+      dispatch(addFeed(res.data.feed));
     } catch (error) {
       if (error.status === 401) {
         navigate('/');
@@ -28,11 +28,12 @@ const Feed = () => {
   useEffect(()=>{
     getFeed();
   },[]);
+
+  if(feed?.length <= 0) return <h1 className="flex justify-center my-10 text-bold text-3xl">Your Feed is Empty</h1>
+
   return ( feed && (
     <div className='flex justify-center my-10 gap-10 p-10'>
-      {feed.feed.map((user,index)=>(
-        <UserCard key={index || user._id} user={user}/>
-      ))}
+      <UserCard user={feed[0]}/>
     </div>
     )
   )
